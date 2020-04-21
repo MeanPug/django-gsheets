47e5995ad20f58c7705eaf6cad32813d6ee38bf5
Bobby Steinbach <steinbach.rj@gmail.com>
2020-04-20 22:11:07 -0400

    initial commit containing dev tooling and setup.py

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.6.7-alpine3.7
+
+RUN mkdir /code
+
+WORKDIR /code
+
+RUN apk add postgresql-dev libffi-dev build-base musl-dev
+RUN apk add linux-headers
+
+ADD requirements.txt .
+
+RUN pip install -r requirements.txt
+
+EXPOSE 3031
+
+ADD dev .
+COPY gsheets ./gsheets
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-gsheets",
+    version="0.0.1",
+    author="Bobby Steinbach",
+    author_email="developers@meanpug.com",
+    description="Django app providing two-way sync from Google Sheets to Django models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MeanPug/django-gsheets",
+    keywords='django google-sheets spreadsheets',
+    packages=setuptools.find_packages(include=['gsheets', 'gsheets.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['peppercorn'],
+    python_requires='>=3',
+)
