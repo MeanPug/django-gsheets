ea9cae4f3b1fb208795741d123b5267a29c2b918
Bobby Steinbach <steinbach.rj@gmail.com>
2020-06-27 12:36:41 -0400

    bumps version to 0.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-gsheets",
-    version="0.0.5",
+    version="0.0.6",
     author="Bobby Steinbach",
     author_email="developers@meanpug.com",
     description="Django app providing two-way sync from Google Sheets to Django models",
