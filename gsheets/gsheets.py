f99d6c1ad72b60287b26fcd8fd3ea76d14125b7e
titorat <abdallahamr5@gmail.com>
2020-07-25 11:38:07 +0200

    testing BOOL to Bool

--- gsheets/gsheets.py
+++ gsheets/gsheets.py
@@ -340,6 +340,13 @@ class SheetPullInterface(BaseSheetInterface):
             field: getattr(self.model_cls, f'clean_{field}_data')(value) if hasattr(self.model_cls, f'clean_{field}_data') else value
             for field, value in data.items() if field != self.sheet_id_field and field in model_fields
         }
+        copy_of_cleaned_data = cleaned_data.copy()
+        for i , j in copy_of_cleaned_data.items():
+            if j.lower() == 'false':
+                cleaned_data[i] = 'False'
+                continue
+            if j.lower() == 'true':
+                cleaned_data[i] = 'True'
         print('THIS IS for debugging puproses')
         print(cleaned_data)
         try:
